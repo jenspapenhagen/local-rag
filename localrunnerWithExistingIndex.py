4b986beaf6f91b6cba70f5256fd2f1a55cf8d370
Jens Papenhagen <jens.papenhagen@web.de>
2023-12-26 21:53:51 +0100

    Update localrunnerWithExistingIndex.py

--- localrunnerWithExistingIndex.py
+++ localrunnerWithExistingIndex.py
@@ -6,16 +6,16 @@ from llama_index import (
 from llama_index.llms import Ollama
 from llama_index.vector_stores.qdrant import QdrantVectorStore
 
-//connet to the filled DB
+#connet to the filled DB
 client = qdrant_client.QdrantClient(
     path="./qdrant_data"
 )
 vector_store = QdrantVectorStore(client=client, collection_name="tweets")
-//build the service context
+#build the service context
 llm = Ollama(model="mixtral")
 service_context = ServiceContext.from_defaults(llm=llm,embed_model="local")
 
-//and query it
+#and query it
 index = VectorStoreIndex.from_vector_store(vector_store=vector_store,service_context=service_context)
 query_engine = index.as_query_engine(similarity_top_k=20)
 response = query_engine.query("Does the author like SQL? Give details.")
